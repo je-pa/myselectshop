50c39106cb14ff57557f6de08678d28d8a7a2374
je-pa <jieun0502@gmail.com>
2025-02-04 00:41:39 +0900

    상품 폴더 설계

---
+++ src/main/java/com/sp/myselectshop/repository/FolderRepository.java
@@ -0,0 +1,8 @@
+package com.sp.myselectshop.repository;
+
+import com.sp.myselectshop.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+
+}
---
+++ src/main/java/com/sp/myselectshop/repository/ProductFolderRepository.java
@@ -0,0 +1,8 @@
+package com.sp.myselectshop.repository;
+
+import com.sp.myselectshop.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+
+}
