f8fe19b54b2b208da9ff7cf77948c90edb02c064
je-pa <jieun0502@gmail.com>
2025-02-04 15:02:11 +0900

    폴더 별 관심상품 조회 구현

--- src/main/java/com/sp/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sp/myselectshop/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.sp.myselectshop.repository;
 
+import com.sp.myselectshop.dto.ProductResponseDto;
 import com.sp.myselectshop.entity.Product;
 import com.sp.myselectshop.entity.User;
 import org.springframework.data.domain.Page;
@@ -9,4 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
   Page<Product> findAllByUser(User user, Pageable pageable);
+
+  // selct * from product p list join product_folder pf on p.id = pf.product_id where p.user_id = 1 and pf.folder_id = 3;
+  Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
